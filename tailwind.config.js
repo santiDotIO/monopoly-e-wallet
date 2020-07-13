af70b24c555ba095d09e492ffa78b07acb8aa802
Santiago J Sosa <me@santisosa.com>
2020-07-12 23:32:03 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('@tailwindcss/ui'),
+  ],
+}
