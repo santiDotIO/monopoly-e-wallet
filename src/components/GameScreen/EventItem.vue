4edde481d296579587cca75342d86fa74814f66a
Santiago J Sosa <me@santisosa.com>
2020-07-31 11:52:44 -0400

    [notification-updates] fixed first event to top

---
+++ src/components/GameScreen/EventItem.vue
@@ -0,0 +1,26 @@
+<template>
+<div v-bind:class="[fixed && 'fixed top-0 left-0 w-full']" class="flex justify-center px-4 py-2 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
+    <div class="w-full shadow-lg rounded-lg pointer-events-auto bg-white">
+        <div class="flex rounded-lg shadow-xs">
+            <div class="flex items-center p-2">
+                <p class="text-sm border-r-2 border-gray-900 pr-1 leading-none font-medium text-gray-900" >{{ new Date(event.data().time_created) | dateFormat('hh:mm:ssA') }}</p>
+                <p class="ml-1 text-sm leading-none text-black" v-html="event.data().text" />
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'EventItem',
+    props: {
+        event: Object,
+        fixed: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    methods:{}
+}
+</script>
--- src/components/GameScreen/EventLog.vue
+++ src/components/GameScreen/EventLog.vue
@@ -1,23 +1,25 @@
 <template>
 <div class="bg-blue-100 mx-4 py-4 rounded shadow-inner overflow-y-scroll h-64">
     <p class="px-4 text-xl md:text-2xl leading-none font-extrabold text-gray-900">Event Log</p>
-    <div v-for="event in events" :key="event.id" class="flex justify-center px-4 py-2 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
-        <!-- v-bind:class="event.data().color" -->
-        <div class="w-full shadow-lg rounded-lg pointer-events-auto">
-            <div class="flex rounded-lg shadow-xs">
-                <div class="flex items-center p-2">
-                    <p class="text-sm border-r-2 border-gray-900 pr-1 leading-none font-medium text-gray-900" >{{ new Date(event.data().time_created) | dateFormat('hh:mm:ssA') }}</p>
-                    <p class="ml-1 text-sm leading-none text-black" v-html="event.data().text" />
-            </div>
-        </div>
-    </div>
-</div>
-
+    <EventItem 
+        v-bind:event="events[0]"
+        v-bind:fixed="true"
+    />
+    <EventItem 
+        v-for="event in events" 
+        v-bind:key="event.id"  
+        v-bind:event="event"
+    />
 </div>
 </template>
 <script>
+import EventItem from './EventItem';
+
 export default {
     name: 'EventLog',
+    components:{ 
+        EventItem 
+    },
     props: {
         events: Array,
     },
