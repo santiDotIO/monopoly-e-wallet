46c77849cc41b64c0c253fce20472429dca77cc0
Santiago J Sosa <me@santisosa.com>
2020-07-30 21:17:03 -0400

    [notification-updates] properly use fotmatter

--- src/components/GameScreen/EventLog.vue
+++ src/components/GameScreen/EventLog.vue
@@ -6,8 +6,7 @@
         <div class="w-full shadow-lg rounded-lg pointer-events-auto">
             <div class="flex rounded-lg shadow-xs">
                 <div class="flex items-center p-2">
-                    <p class="text-sm border-r-2 border-gray-900 pr-1 leading-none font-medium text-gray-900" >{{ event.data().time_created | dateParse('hh:mm:ss A') }}</p>
-                    
+                    <p class="text-sm border-r-2 border-gray-900 pr-1 leading-none font-medium text-gray-900" >{{ new Date(event.data().time_created) | dateFormat('hh:mm:ssA') }}</p>
                     <p class="ml-1 text-sm leading-none text-black" v-html="event.data().text" />
             </div>
         </div>
